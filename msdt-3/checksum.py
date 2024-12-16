23f53b46d2129ac0f87c2f1707abd68e1807873b
van <vexyzy@yandex.ru>
2024-12-16 15:10:12 +0400

    Add csv document validation func in checksum.py. Add css file. Refactor serialize result in checksum.py

--- msdt-3/checksum.py
+++ msdt-3/checksum.py
@@ -1,41 +1,89 @@
+"""
+В этом модуле обитают функции, необходимые
+для автоматизированной проверки результатов ваших трудов.
+"""
+
 import json
 import hashlib
 from typing import List
+
+# import csv package for work with csv file
+from csv import DictReader
+
 # import validator to checking input fields using regexes
 from extensions import validator
 
-"""
-В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
-"""
 
-def type_to_regex(input_case: str, input_field: str) -> None:
-    
+CSV_PATH = "./20.csv"
+JSON_PATH = "./result.json"
+OPTION = 20
+
+
+def get_numbers_with_mistakes(csv_path: str):
+    """This method return numbers where format is wrong in some of fields
+
+    Args:
+        csv_path (str): path to csv file
+    Returns:
+        numbers_with_mistake (List[int]): numbers with format of data
+    """
+    numbers_with_mistakes = []
+
+    with open(csv_path, newline="", encoding="utf-16") as csv_file:
+        dict_reader = DictReader(csv_file, delimiter=";")
+        for row_num, row in enumerate(dict_reader):
+            for pattern, data in row.items():
+                if not validator.validate_data(pattern, data):
+                    numbers_with_mistakes.append(row_num - 1)
+
+    return numbers_with_mistakes
+
 
 def calculate_checksum(row_numbers: List[int]) -> str:
     """
     Вычисляет md5 хеш от списка целочисленных значений.
 
-    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая   строка с данными csv-файла имеет номер 0
-    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
+    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая
+    строка с данными csv-файла имеет номер 0
+    Другими словами: В исходном csv 1я строка - заголовки столбцов,
+    2я и остальные - данные.
+    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер
+    1 и так далее.
 
-    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
+    :param row_numbers: список целочисленных номеров строк csv-файла,
+    на которых были найдены ошибки валидации
     :return: md5 хеш для проверки через github action
     """
     row_numbers.sort()
-    return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
+    return hashlib.md5(json.dumps(row_numbers).encode("utf-8")).hexdigest()
 
 
 def serialize_result(variant: int, checksum: str) -> None:
     """
     Метод для сериализации результатов лабораторной пишите сами.
-    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
+    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл,
+    лежащий в папке с лабораторной.
     Файл называется, очевидно, result.json.
 
-    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
+    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет
+    корректность выполнения лабораторной.
+    Так что не перемещайте, не переименовывайте и не изменяйте его структуру,
+    если планируете успешно сдать лабу.
 
     :param variant: номер вашего варианта
     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
     """
-    pass
+    result = {"variant": variant, "checksum": checksum}
+    with open(JSON_PATH, "w", encoding="utf-8") as json_file:
+        json.dump(result, json_file)
+
+
+def main():
+    """Entry point"""
+    numbers_with_mistakes = get_numbers_with_mistakes(CSV_PATH)
+    check_sum = calculate_checksum(numbers_with_mistakes)
+    serialize_result(OPTION, check_sum)
+
+
+if __name__ == "__main__":
+    main()
--- msdt-3/validator.py
+++ msdt-3/validator.py
@@ -1,6 +1,6 @@
 """
-    class validator
-    This module validate data for some regex pattern
+class validator
+This module validate data for some regex pattern
 """
 
 # Import match for matching strings to regex
@@ -11,26 +11,27 @@ patterns = {
     "email": r"^\w+@\w+(\.\w+)+$",
     "inn": R"^\d{12}$",
     "passport": r"^\d{2}\s\d{2}\s\d{6}$",
-    "ipv4": (r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}"
-                + r"([09]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"),
+    "ipv4": (
+        r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}"
+        + r"([09]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    ),
     "latitude": r"^[-\d]\d*(\.\d+)?$",
     "rgb": r"^#[\dA-Fa-f]{6}$",
     "isbn": r"(^\d{3}-)?\d-\d{5}-\d{3}-\d$",
     "uuid": (
-        (r"^[\dafA-F]{8}-[\da-fA-F]{4}-"
-        + r"[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}$")
+        r"^[\dafA-F]{8}-[\da-fA-F]{4}-" + r"[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}$"
     ),
     "time": r"^([0-1][0-9]|[2][0-3]):[0-5][0-9]:[0-5][0-9]\.[0-9]{6}$",
-    "telephone": r"^\+7-\(\d{3}\)-\d{3}(-\d{2}){2}$", # телефончик
-    "height": r"^[0-2]\.\d{2}$", # рост
-    "snils": r"^\d{11}$", # снилс
-    "identifier": r"^\d{2}-\d{2}\/\d{2}$", # id
-    "occupation": r"^[A-Za-zА-Яа-я- ]+$", # профессия
-    "longitude": r"^[(\-\d)\d]\d*\.\d*$", # долгота
-    "blood_type": r"^([ABO]|AB)[\−\+]$", # кровушка
-    "issn": r"^\d{4}\-\d{4}$", # issn
-    "locale_code": r"^[A-Za-z]+(\-[A-Za-z]+)*$", # locale code
-    "date": r"^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$" # data
+    "telephone": r"^\+7-\(\d{3}\)-\d{3}(-\d{2}){2}$",  # телефончик
+    "height": r"^[0-2]\.\d{2}$",  # рост
+    "snils": r"^\d{11}$",  # снилс
+    "identifier": r"^\d{2}-\d{2}\/\d{2}$",  # id
+    "occupation": r"^[A-Za-zА-Яа-я- ]+$",  # профессия
+    "longitude": r"^[(\-\d)\d]\d*\.\d*$",  # долгота
+    "blood_type": r"^([ABO]|AB)[\−\+]$",  # кровушка
+    "issn": r"^\d{4}\-\d{4}$",  # issn
+    "locale_code": r"^[A-Za-z]+(\-[A-Za-z]+)*$",  # locale code
+    "date": r"^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$",  # data
 }
 
 MAX_LONGITUDE = 180
@@ -40,21 +41,15 @@ MIN_LATITUDE = -90
 MAX_LATITUDE = 90
 
 
-
 class Validator:
     """
     This class has methods for validating data.
     """
 
     def __init__(self):
-            pass
+        pass
 
-
-    def validate_data(
-            self,
-            regex_pattern: str,
-            input_data: str
-            ) -> bool:
+    def validate_data(self, regex_pattern: str, input_data: str) -> bool:
         """This method validate data for pattern
 
         Args:
